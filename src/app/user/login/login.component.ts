7b946e5c52598f541808254fb9791cdfb9d2811d
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2022-04-10 18:50:57 -0300

    implementando a diretiva ngModel em login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  credentials = {
+    email: '',
+    password: '',
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login() {
+    console.log(this.credentials);
   }
-
 }
