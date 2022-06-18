0e5467acc29cdec6be916a2b881388d91782aab3
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2022-06-18 20:56:33 -0300

    verifica se o observable de autenticação para exibir navbar

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 import { ModalService } from '../services/modal.service';
 
 @Component({
@@ -7,7 +8,7 @@ import { ModalService } from '../services/modal.service';
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
-  constructor(public modal: ModalService) { }
+  constructor(public modal: ModalService, public auth: AuthService) {}
 
   openModal($event: Event) {
     $event.preventDefault();
