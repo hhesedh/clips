0f58ee19d95c34d4cdb99251b22703c27aeb3318
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2022-06-18 17:42:02 -0300

    linkando informações do usuário com o da criação de login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,7 +12,7 @@ import IUser from '../models/user.model';
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
-    this.usersCollection = db.collection('users');
+    this.usersCollection = this.db.collection('users');
   }
 
   public async createUser(userData: IUser) {
@@ -23,11 +23,20 @@ export class AuthService {
       userData.email,
       userData.password
     );
-    await this.usersCollection.add({
+
+    if (!userCred.user) {
+      throw new Error("User can't be found");
+    }
+
+    await this.usersCollection.doc(userCred.user?.uid).set({
       name: userData.name,
       email: userData.email,
       age: userData.age,
       phoneNumber: userData.phoneNumber,
+    });
+
+    await userCred.user.updateProfile({
+      displayName: userData.name,
     });
   }
 }
