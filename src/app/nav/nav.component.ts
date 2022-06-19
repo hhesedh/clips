e8fd1051b5b0f7fd4efeb7fc330784e3f75a33bb
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2022-06-19 15:07:57 -0300

    delegando função de logout para o AuthService

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,29 +1,17 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { ModalService } from '../services/modal.service';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
-  constructor(
-    public modal: ModalService,
-    public auth: AuthService,
-    private afAuth: AngularFireAuth,
-    private router: Router
-  ) {}
+  constructor(public modal: ModalService, public auth: AuthService) {}
 
   openModal($event: Event) {
     $event.preventDefault();
     this.modal.toogleModal('auth');
-  }
-
-  async logout($event: Event) {
-    $event.preventDefault();
-    await this.afAuth.signOut();
-    await this.router.navigateByUrl('/');
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,6 +7,7 @@ import {
 import { Observable } from 'rxjs';
 import { map, delay } from 'rxjs/operators';
 import IUser from '../models/user.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -16,7 +17,11 @@ export class AuthService {
   public isAuthenticated$: Observable<boolean>;
   public isAuthenticatedWithDelay$: Observable<boolean>;
 
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+  constructor(
+    private auth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router
+  ) {
     this.usersCollection = this.db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(map((user) => !!user));
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
@@ -45,5 +50,13 @@ export class AuthService {
     await userCred.user.updateProfile({
       displayName: userData.name,
     });
+  }
+
+  async logout($event?: Event) {
+    if ($event) {
+      $event.preventDefault();
+    }
+    await this.auth.signOut();
+    await this.router.navigateByUrl('/');
   }
 }
