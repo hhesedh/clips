c2379820ab8c9132ce60b6f4f4af06a8be816102
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2022-06-18 22:02:03 -0300

    implementação de botão de logout

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { ModalService } from '../services/modal.service';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
-  constructor(public modal: ModalService, public auth: AuthService) {}
+  constructor(
+    public modal: ModalService,
+    public auth: AuthService,
+    private afAuth: AngularFireAuth
+  ) {}
 
   openModal($event: Event) {
     $event.preventDefault();
     this.modal.toogleModal('auth');
+  }
+
+  async logout($event: Event) {
+    $event.preventDefault();
+    await this.afAuth.signOut();
   }
 }
